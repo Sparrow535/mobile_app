9d433a6dab9a8d17856bcc6f369ad4fde9083f20
Sparrow535 <rohanrai4133@gmail.com>
2025-07-10 14:57:57 +0600

    feat: Add movie card and search bar components, integrate TMDB API for movie fetching
    - Implemented MovieCard component for displaying movie details with navigation.
    - Created SearchBar component for user input with a search icon.
    - Added icons and images constants for better asset management.
    - Defined interfaces for Movie and MovieDetails to ensure type safety.
    - Configured Babel and Metro for NativeWind support.
    - Established API service for fetching movies from TMDB.
    - Developed custom hook `useFetch` for handling API requests and state management.
    - Configured Tailwind CSS for styling with NativeWind.
    - Added TypeScript declarations for PNG imports.

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,17 @@
+import { images } from "@/constants/images";
+import React from "react";
+import { Image, View } from "react-native";
+
+const profile = () => {
+  return (
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+    </View>
+  );
+};
+
+export default profile;
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,59 @@
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import React from "react";
+import { ActivityIndicator, FlatList, Image, View } from "react-native";
+
+const Search = () => {
+  const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
+  return (
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar placeholder="Search for a movie" />
+            </View>
+            {moviesLoading && (
+              <ActivityIndicator size="large" color="#0000ff" />
+            )}
+          </>
+        }
+      />
+    </View>
+  );
+};
+
+export default Search;
