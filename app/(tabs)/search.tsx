bfdd060d868fc3314abcffff6a0e4aadf63ce8ca
Sparrow535 <rohanrai4133@gmail.com>
2025-07-10 18:01:24 +0600

    feat: Implement search functionality with Appwrite integration and update SearchBar component

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,22 +3,43 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
-import { useRouter } from "expo-router";
-import React from "react";
-import { ActivityIndicator, FlatList, Image, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 const Search = () => {
-  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
+
   const {
     data: movies,
-    loading: moviesLoading,
-    error: moviesError,
-  } = useFetch(() =>
-    fetchMovies({
-      query: "",
-    })
+    loading,
+    error,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(
+    () =>
+      fetchMovies({
+        query: searchQuery,
+      }),
+    false
   );
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+
+        if (movies?.length > 0 && movies?.[0])
+          await updateSearchCount(searchQuery, movies[0]);
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
   return (
     <View className="flex-1 bg-primary">
       <Image
@@ -44,12 +65,36 @@ const Search = () => {
               <Image source={icons.logo} className="w-12 h-10" />
             </View>
             <View className="my-5">
-              <SearchBar placeholder="Search for a movie" />
+              <SearchBar
+                placeholder="Search for a movie"
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
             </View>
-            {moviesLoading && (
-              <ActivityIndicator size="large" color="#0000ff" />
+            {loading && <ActivityIndicator size="large" color="#0000ff" />}
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
+              <Text className="text-xl text-white font-bold">
+                Search Results for {` `}
+                <Text className="text-accent">{searchQuery}</Text>
+              </Text>
             )}
           </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim()
+                  ? "No results found"
+                  : "Start searching for movies"}
+              </Text>
+            </View>
+          ) : null
         }
       />
     </View>
